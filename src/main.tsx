f37b67ef51c705c89a492daa7a144c3f2ee29578
harikk1506 <harikarthikeyan43@gmail.com>
2025-10-09 00:12:59 +0530

    Fix: Runtime crash by explicitly importing StrictMode in src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { StrictMode } from 'react' // MODIFIED: Explicitly importing StrictMode
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // MODIFIED: Using StrictMode component directly
+  <StrictMode> 
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
